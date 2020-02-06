ad50ca5881dcdcc424674e11cabaef0b32f4f345
Marcin Mik <piptrik@poczta.fm>
2020-02-06 21:59:36 +0100

    Java 13 features evaluation

---
+++ src/test/java/evaluation/java/thirteen/ConvenienceImprovements.java
@@ -0,0 +1,64 @@
+package evaluation.java.thirteen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConvenienceImprovements {
+
+    @Test
+    public void textBlockGivesTheSameResultAsOldStringBuilding() {
+        String textBlock = """
+                Some
+                multiline
+                String""";
+        String oldMultilineString = "Some\nmultiline\nString";
+
+        assertEquals(oldMultilineString, textBlock);
+    }
+
+    @Test
+    public void formattedStrings() {
+        String textBlockToFormat = """
+               First value: %s
+               Second value: %s""";
+
+        assertEquals(String.format(textBlockToFormat, "'1'", "'2'"), textBlockToFormat.formatted("'1'", "'2'"));
+    }
+
+    @Test
+    public void enhancedSwitch() {
+        assertEquals(oldSwitch(TestEnum.FIRST), newSwitch(TestEnum.FIRST));
+        assertEquals(oldSwitch(TestEnum.SECOND), newSwitch(TestEnum.SECOND));
+        assertEquals(oldSwitch(TestEnum.THIRD), newSwitch(TestEnum.THIRD));
+        assertEquals(oldSwitch(TestEnum.OTHER), newSwitch(TestEnum.OTHER));
+    }
+
+
+
+    private double newSwitch(TestEnum test) {
+        return switch (test) {
+            case FIRST -> 1;
+            case SECOND -> 2;
+            case THIRD -> 3;
+            default -> 0;
+        };
+    }
+
+    private double oldSwitch(TestEnum test) {
+        switch(test) {
+            case FIRST:
+                return 1;
+            case SECOND:
+                return 2;
+            case THIRD:
+                return 3;
+            default:
+                return 0;
+        }
+    }
+
+    enum TestEnum {
+        FIRST,SECOND,THIRD, OTHER
+    }
+}
